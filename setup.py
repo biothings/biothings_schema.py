b233cc2f4e648bfd6135ad800cf7cab2da258fc8
sookieliao <kevinxin@scripps.edu>
2019-05-07 12:31:19 -0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+setup(
+    name="biothings_schema",
+    version="0.0.1",
+    author="Jiwen Xin, Chunlei Wu",
+    author_email="cwu@scripps.edu",
+    description="Python Client for BioThings Schema",
+    license="BSD",
+    keywords="schema biothings",
+    url="https://github.com/biothings/biothings_schema.py",
+    packages=['biothings_schema'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'graphviz==0.10.1',
+        'jsonschema==3.0.1',
+        'networkx==2.3'
+    ],
+)
