56ba9fcf51ebfaf502b9538ed40938b58ed0b1fc
Chunlei Wu <anewgene@yahoo.com>
2022-01-20 20:40:08 -0800

    build: :arrow_up: updated the dependecies

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup
 
 install_requires = [
     'jsonschema>=3.0.1',
-    'networkx==2.6',
+    'networkx==2.6.3',
     'PyYAML>=5.1',
-    'requests>=2.21.0',
-    'strict-rfc3339'
+    'requests>=2.26.0',
+    'strict-rfc3339>=0.7'
 ]
 
 extra_requires = [
-    'graphviz>=0.10.1'
+    'graphviz>=0.19.1'
 ]
 
 setup(
     name="biothings_schema",
-    version="0.0.1",
+    version="0.0.2",
     author="Jiwen Xin, Chunlei Wu",
     author_email="cwu@scripps.edu",
     description="Python Client for BioThings Schema",
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
