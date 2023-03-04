2467c49d1925bb763db3c78874910e7e1746e6ce
Chunlei Wu <anewgene@yahoo.com>
2023-03-04 10:26:20 -0800

    style: code style fixes

--- biothings_schema/tests/time.py
+++ biothings_schema/tests/time.py
@@ -1,29 +1,32 @@
-import time
+"""timing a schema parsing"""
+from time import time
 
 from biothings_schema import Schema
 
 
 def timeit():
-    start = time.time()
-    PATH = 'https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie_kevin.jsonld'
-    se = Schema(PATH)
-    clses = se.list_all_classes()
+    """timing a schema parsing"""
+    start = time()
+    bts_url = "https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie_kevin.jsonld"
+    bts_se = Schema(bts_url)
+    clses = bts_se.list_all_classes()
     for _cls in clses:
-        es_class = {'schema': None, 'name': None, 'clses': None, 'props': []}
-        es_class['schema'] = _cls.prefix
-        es_class['name'] = _cls.label
-        es_class['clses'] = [', '.join(map(str, schemas))
-                             for schemas in _cls.parent_classes]
+        es_class = {"schema": None, "name": None, "clses": None, "props": []}
+        es_class["schema"] = _cls.prefix
+        es_class["name"] = _cls.label
+        es_class["clses"] = [", ".join(map(str, schemas)) for schemas in _cls.parent_classes]
         for prop in _cls.list_properties(group_by_class=False):
             info = prop.describe()
-            _property = {'name': str(prop),
-                         'value_types': [str(_type) for _type in info['range']],
-                         'description': info.get('description')}
+            _property = {
+                "name": str(prop),
+                "value_types": [str(_type) for _type in info["range"]],
+                "description": info.get("description"),
+            }
 
-            es_class['props'].append(_property)
-    end = time.time()
+            es_class["props"].append(_property)
+    end = time()
     print(end - start)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     timeit()
